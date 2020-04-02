5006fdeea6dc7494c73cf4728740ec1c692bc9da
kkeus <59583228+kkeus@users.noreply.github.com>
2020-04-02 12:14:46 -0700

    Add files via upload

---
+++ phasespace.m
@@ -0,0 +1,15 @@
+figure; scatter(Mgamma_average,TD_ratio)
+
+phasestates = Mstates;
+phasestates(phasestates == 1) = 1;
+phasestates(phasestates == 2) = 2;
+phasestates(phasestates == 3) = 3;
+phasestates(phasestates == 4) = 1;
+phasestates(phasestates == 5) = 1;
+phasestates(phasestates == 6) = 2;
+phasestates(phasestates == 7) = 7;
+
+figure; gscatter(Mgamma_average,TD_ratio,phasestates)
+set(gca,'Color','k')
+
+% figure; gscatter(EMGave,Mgamma_average,manual_output)
